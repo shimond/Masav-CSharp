5ef27485e39dd7fdb30aa622f59064c2a51e1d8d
Shimon Dahan <shimond@any-techs.co.il>
2025-12-29 10:27:47 +0200

    Object and Basic methods

--- Day2/OOP_Object/Program.cs
+++ Day2/OOP_Object/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace OOP_Object
 {
@@ -6,21 +7,68 @@ namespace OOP_Object
     {
         static void Main(string[] args)
         {
-            Person person = new Person("John Doe");
-            //person.FullName = "John Doe";
-            person.PrintInfo();
+            //Person person = new Person("John Doe");
+            ////person.FullName = "John Doe";
+            //person.PrintInfo();
 
-            //Person anotherPerson = new Person("Jane", "Smith");
-            Person anotherPerson = new Person(firstName:"Jane", lastName: "Smith");
-            //anotherPerson.FullName = "Jane Smith";
-            anotherPerson.PrintInfo();
+            ////Person anotherPerson = new Person("Jane", "Smith");
+            //Person anotherPerson = new Person(firstName:"Jane", lastName: "Smith");
+            ////anotherPerson.FullName = "Jane Smith";
+            //anotherPerson.PrintInfo();
+
+            object p = new Person()
+            {
+                //Id = 1,
+                FullName = "Moshe Cohen"
+            };
+
+
+
+            if (p is Person)
+            {
+                ((Person)p).PrintInfo();
+            }
+
+            var p1 = p as Person;
+            if(p1 != null)
+            {
+                p1.PrintInfo();
+            }
+
+
+
+
+
+
+
+
+
         }
+        static void Test(Person p)
+        {
+            p.PrintInfo();
+        }
+
+        static void Test(object p)
+        {
+            Console.WriteLine(p.ToString());
+        }
+
+        static void Test(Worker p)
+        {
+            p.PrintInfo();
+        }
+
     }
 
-    public class Person
+    public class Person : Object
     {
+        protected int ProtectedField { get; set; }
         private string _firstName;
         private string _lastName;
+
+
+        //public required int Id { get; set; }
         public string FullName
         {
             get
@@ -44,22 +92,33 @@ namespace OOP_Object
             }
         }
 
-        public void PrintInfo()
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public virtual void PrintInfo()
         {
             Console.WriteLine("Full Name: " + FullName);
         }
 
-        public Person() : this("NO_FIRST_NAME","NO_LAST_NAME")
+        public Person() : this("NO_FIRST_NAME", "NO_LAST_NAME")
         {
-                
+
         }
+
 
         public Person(string fullName)
         {
             this.FullName = fullName;
         }
 
-        public Person(string firstName, string lastName): this(firstName + " " + lastName)
+        public Person(string firstName, string lastName) : this(firstName + " " + lastName)
         {
             //this._firstName = firstName;
             //this._lastName = lastName;
@@ -72,6 +131,17 @@ namespace OOP_Object
         //}
 
 
+    }
+
+
+    public class Worker : Person
+    {
+        public string CompanyName { get; set; }
+        public sealed override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine("Company Name: " + CompanyName);
+        }
     }
 
     //v1 - bad design
